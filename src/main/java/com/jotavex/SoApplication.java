30d14d963bcec15c336d6203c5ad6369c74450f4
Jotavex <joaovictorbronzatti@gmail.com>
2018-09-24 00:59:43 -0300

    Projeto Criado

---
+++ src/main/java/com/jotavex/SoApplication.java
@@ -0,0 +1,12 @@
+package com.jotavex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jotavex/SoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jotavex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
